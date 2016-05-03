bf7764dd1a78146f085b80401f3d007f4b5995c3
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-03 19:59:33 +0400

    + typical bI engine
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ hpp.hpp
@@ -0,0 +1,33 @@
+#ifndef _H_IP
+#define _H_IP
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string tag,val;
+	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string dump(int=0);
+	virtual string tagval(); string pad(int);
+};
+
+extern map<string,Sym*> env;
+extern void env_init();
+
+struct List: Sym { List(); };
+
+struct Op: Sym { Op(string); };
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_IP
